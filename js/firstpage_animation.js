9c28beed431dc5e74c11e22c70ad2361d0e35335
Root-moody <moodyamin531@gmail.com>
2021-08-21 13:39:42 +0200

    Portfolio

---
+++ js/firstpage_animation.js
@@ -0,0 +1,13 @@
+// function ScrollAppear() {
+//     var appearingtext;
+//     appearingtext = document.querySelector('.webdesign_container');
+//     let textposition = appearingtext.getBoundingClientRect().top;
+//     let screenposition = window.innerHeight / 2;
+
+//     if (textposition < screenposition) {
+//         appearingtext.classList.add('text_appear');
+//     }
+// }
+
+
+// window.addEventListener('scroll', ScrollAppear);
